3f2beee8512ed8042c149ac4a9e56159d99b8b18
Punit_singh <mr.punitsingh2628@gmail.com>
2026-01-11 13:19:47 +0530

    Add files via upload

---
+++ data-type.js
@@ -0,0 +1,63 @@
+//PD
+var str = 'Punit';
+
+var bool = true;
+
+var und = undefined;
+
+var nul = null;
+
+var sym = Symbol();
+
+var largeNumber = 9821718888; //bigInt
+
+//non PD
+
+var obj = {name: 'Punit', age:21, address:'Mira road'};
+
+var arr = [1, 2, 3, 'Punit', 'Singh', {name: 'India'}, true, false]; //array
+
+var arrOfObject = [{str: 'name'}, {number: 2}]; //array of object with mx
+
+function hello(){
+    console.log('Punit  Singh');
+}
+
+//Operators
+
+let n1 = 2;
+
+let n2 = 4;
+
+console.log("Sum:", n1 + n2);
+console.log("Sub", n1 - n2);
+console.log("mul", n1 * n2);
+console.log("div", n1 / n2);
+console.log("exp", n1 ** n2);
+
+//Assign
+
+var name1 = "Punit";
+
+var name2 = "Singh";
+
+//Camparison
+
+let num1 = 3;
+
+let num2 = '3';
+
+let ans = num1 == num2;
+
+console.log("Value of 3 == '3': ",ans); //only camparison between values
+
+let answer = num1 === num2;
+console.log("Value of 3 === '3': ",answer); // Only camparison between data values and types
+
+console.log("Value of 3 !== '3': ",!answer) // Only camparison between 
+
+
+
+
+
+
