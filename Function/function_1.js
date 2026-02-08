432faa6cf87ca77fe9e3eadc098dfe10ce11e115
Punit_singh <mr.punitsingh2628@gmail.com>
2026-02-08 13:53:54 +0530

    Add files via upload
    Function Practice

---
+++ Function/function_1.js
@@ -0,0 +1,53 @@
+// // function 3 steps to follow 
+// // 1- Define 2- Declaration 3- Calling 
+
+// function firstSum(){
+//     console.log(2+3);
+// }
+// firstSum();
+
+
+// const arrowFunction = () => {
+//     console.log(5+5);
+// };
+
+// arrowFunction();
+
+// // Function Return Type
+
+// function paraFun(a, b){
+//     // console.log(a+b);
+//     return a+b;
+// }
+// paraFun(5,9);
+// const ans = paraFun(2,3);
+// console.log(ans);
+
+let num_1 = +process.argv[2];
+console.log('num_1: ',num_1);
+let num_2 = +process.argv[3];
+console.log('num_2: ',num_2);
+
+const sum = (a, b) => {
+    return a + b;
+}
+const sub = (a, b) => {
+    return a - b;
+}
+const mul = (a, b) => {
+    return a * b;
+}
+const div = (a, b) => {
+    if(b <= 0) {
+        return 'please insert proper input';
+    }
+    return a / b;
+}
+const ans_1 = sum(num_1, num_2);
+const ans_2 = sub(num_1, num_2);
+const ans_3 = mul(num_1, num_2);
+const ans_4 = div(num_1, num_2);
+console.log('Addition of two number: ',ans_1);
+console.log('Subtraction of two number: ',ans_2);
+console.log('Multiplication of two number: ',ans_3);
+console.log('Division of two number: ',ans_4);
---
+++ Function/html-fun/script.js
@@ -0,0 +1,27 @@
+let num_1 = prompt("Enter num-1");
+console.log('num_1: ',num_1);
+let num_2 =  prompt("Enter num-2");
+console.log('num_2: ',num_2);
+const sum = (a, b) => {
+    return a + b;
+}
+const sub = (a, b) => {
+    return a - b;
+}
+const mul = (a, b) => {
+    return a * b;
+}
+const div = (a, b) => {
+    if(b <= 0) {
+        return 'please insert proper input';
+    }
+    return a / b;
+}
+const ans_1 = sum(num_1, num_2);
+const ans_2 = sub(num_1, num_2);
+const ans_3 = mul(num_1, num_2);
+const ans_4 = div(num_1, num_2);
+console.log('Addition of two number: ',ans_1);
+console.log('Subtraction of two number: ',ans_2);
+console.log('Multiplication of two number: ',ans_3);
+console.log('Division of two number: ',ans_4);
