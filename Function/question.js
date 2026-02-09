2d7493f4dcf35bbfdb45f8ad36b9010772cf5431
Punit-Singh28 <mr.punitsingh2628@gmail.com>
2026-02-09 20:06:17 +0530

    Practice Questions

--- Function/question.js
+++ Function/question.js
@@ -1,5 +1,5 @@
 
-// Question -01
+// Question 01: Number is Even or Odd
 // let num_1 = +process.argv[2];
 // console.log('num_1: ',num_1);
 
@@ -15,9 +15,9 @@
 // const ans = evenOrOddNumber(num_1);
 // console.log(ans);
 
-// Question -02
-// let num_1 = +process.argv[2];
-// console.log('num_1: ',num_1);
+// Question 02: Number is Positive Negative or Zero
+// let num_2 = +process.argv[2];
+// console.log('num_2: ',num_2);
 // const numberIsPosNeg = (a)=>{
 //     if(a > 0){
 //         return `number is Positive`;
@@ -29,29 +29,62 @@
 //         return `number is Negative`;
 //     }
 // }
-// const ans_1 = numberIsPosNeg(num_1);
+// const ans_1 = numberIsPosNeg(num_2);
 // console.log(ans_1);
 
-// Question 3
+// Question 3: Grade of Student
 
-let num_1 = +process.argv[2];
-console.log('num_1: ',num_1);
- const gradeOfStudent = (a)=>{
-    if(a <=100 && a > 90){
-        return `A Grade`;
-    }
-    else if(a < 90 && a >= 80){
-        return `B Grade`;
-    }
-     else if(a < 80 && a >= 70){
-        return `B Grade`;
-    }
-     else if(a < 70 && a >= 60){
-        return `B Grade`;
-    }
-    else{
-        return `F Grade`;
-    }
-}
-const ans_2 = gradeOfStudent(num_1);
-console.log(ans_2);
+// let num_3 = +process.argv[2];
+// console.log('num_3: ',num_3);
+//  const gradeOfStudent = (a)=>{
+//     if(a <=100 && a > 90){
+//         return `A Grade`;
+//     }
+//     else if(a < 90 && a >= 80){
+//         return `B Grade`;
+//     }
+//      else if(a < 80 && a >= 70){
+//         return `B Grade`;
+//     }
+//      else if(a < 70 && a >= 60){
+//         return `B Grade`;
+//     }
+//     else{
+//         return `F Grade`;
+//     }
+// }
+// const ans_2 = gradeOfStudent(num_3);
+// console.log(ans_2);
+
+// Question 04: Year is leap or not
+
+// let num_4 = process.argv[2];
+// console.log('Your Year is:', num_4);
+// const checkYear = (a) => {
+//     if(a % 4 === 0){
+//         if (a % 100 === 0){
+//             return a % 400 === 0;
+//         }
+//         return true;
+//     }
+//     return false
+// }
+// // let year = 2000;
+// // let ans_3 = checkYear(year);
+// const ans_3 = checkYear(num_4);
+// console.log('leap Year is',ans_3);
+
+// Question 05: Check given number is vowel or consonant
+// const char_1 = 'P';
+// const checkChar = (char) => {
+//     if (char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u' || char == 'A' || char == 'E' || char == 'I' || char == 'O' || char == 'U'){
+//         return `${char} is Vowel`;
+//     }
+//     else{
+//         return `${char} is Consonant`;
+//     }
+// }
+
+// const ans_4 = checkChar(char_1);
+// console.log(ans_4);
+
