7e31e4308c98512fee40031a985b080f6b0d30f6
Punit_singh <mr.punitsingh2628@gmail.com>
2026-02-22 12:23:14 +0530

    Add files via upload
    function Question Practice

---
+++ Function/Function-question/qna.js
@@ -0,0 +1,38 @@
+// Q01. write a func that take two no. and return their sums
+
+// let num1 = +process.argv[2];
+// let num2 = +process.argv[3];
+// const sum = (a, b) => {
+//     return a + b;
+// }
+// let ans = sum(num1, num2);
+// console.log("Your sum value is", ans);
+
+// Q02 write a func that takes number and return its factorial
+// let num = +process.argv[2];
+// const factorial = (a) => {
+//     let b = 1;
+//     for(let i=1; i<= a; i++){
+//         b *= i;
+//     }
+//     return b;
+// }
+// let ans = factorial(num);
+// console.log("Your factorial value is", ans);
+
+// Q03. write a func that takes an array and return summ of all the numbers in arr
+
+// let arr = [50, 50, 100];
+// const sumOfArr = (array) => {
+//     let sum = 0;
+//     for(let i = 0; i < array.length; i++){
+//         console.log(array[i])
+//         console.log("before",sum)
+//         sum += array[i];
+//         console.log("after",sum)
+//     }
+//     return sum;
+// }
+// let ans = sumOfArr(arr);
+// console.log("Sum of all the number in array", ans);
+
