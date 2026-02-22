f5619cc141ca951b42a97a0202959b3fd23e05a7
Punit_singh <mr.punitsingh2628@gmail.com>
2026-02-22 12:24:16 +0530

    Add files via upload
    Loop basic started

---
+++ Function/loops/basic.js
@@ -0,0 +1,27 @@
+// For Loops
+
+let name = "Punit";
+let arr = [11,12,13,14,15,16,17,18,19];
+// for(let i = 1; i < 100; i++){
+//     console.log(name, i);
+// }
+for(let i = 0; i < arr.length; i++){
+    // console.log(arr[i]);
+}
+
+// while loop
+let start = 1;
+let end = 10;
+while (start < end) {
+    // if (start % 2 == 0) console.log("This number is even: ",start);
+    // else console.log("This number is odd: ",start);
+    start++; 
+}
+
+// do while loop
+
+let no = 1;
+do {
+    console.log(no);
+    
+} while (no > 2); //if this condition false loop ends
